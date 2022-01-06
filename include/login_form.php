d848f6e9f268e7e2a9ecf812e72e0fe87add87df
nmarrocchi <nmarrocchi@la-providence.net>
2022-01-06 23:36:31 +0100

    form login include file

---
+++ include/login_form.php
@@ -0,0 +1,10 @@
+<div class="Login_form">
+  <form action="" method="post">
+    <img id="login_icon" src="img/login_icon.png" alt="Login_Icon">
+
+    <input type="text" name="username" placeholder="info@mailadress.com" required>
+    <input type="text" name="password" placeholder="******" required>
+
+    <input type="submit" value="Login">
+  </form>
+</div>
