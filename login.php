817eb975de600369f58d1e84b2757dd2dba39a10
nmarrocchi <nmarrocchi@la-providence.net>
2022-01-06 23:37:24 +0100

    login page including form

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style/index.css">
+  <link rel="stylesheet" href="style/nav.css">
+  <link rel="stylesheet" href="style/login_form.css">
+  <title>Login</title>
+</head>
+<body>
+
+<?php 
+  include "include/nav.php";
+  include "include/login_form.php";
+?>
+  
+</body>
+</html>
