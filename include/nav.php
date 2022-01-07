0669e965810f17c637873cbf5c28ce34c543fece
nmarrocchi <nmarrocchi@la-providence.net>
2022-01-07 16:47:50 +0100

    add login check nav menu

--- include/nav.php
+++ include/nav.php
@@ -1,5 +1,20 @@
 <div class="nav">
-    <a href="login.php"><div class="nav-slot">Login</div></a>
+    <?php
+      if($_SESSION["Logged"] == true)
+      {
+        ?>  
+          <a href="logout.php"><div class="nav-slot">Déconnexion</div> </a> 
+        <?php
+          if($_SESSION["isAdmin"] == 1)
+          {
+            ?>  <a href="admin_panel.php"><div class="nav-slot">Gestion des utilisateurs</div></a>  <?php
+          }
+      }
+      else{
+        ?>  <a href="login.php"><div class="nav-slot">Login</div></a>  <?php
+      }
+    ?>
+
     <a href="system.php"><div class="nav-slot">Supervisation Labo</div></a>
   </ul>
 </div>
