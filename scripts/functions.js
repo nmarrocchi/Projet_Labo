7b4f5786d59deb3cea3e264f8b815cefe0c51ce8
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-24 10:49:36 +0100

    add functions script

---
+++ scripts/functions.js
@@ -0,0 +1,25 @@
+// - Functions Marrocchi
+function formRequest(){}
+
+function getCapteursHisto(){}
+
+function getPassagesHisto(){}
+
+function getAllUsers(){}
+
+function deleteUser(){}
+
+function createUser(){}
+
+function updateUser(){}
+
+function connexion(){}
+
+function disconnection(){}
+
+// - Functions Colson
+function systemParameters(){}
+
+// - Functions Cauet
+function getAllCapteursState(){}
+
