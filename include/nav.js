a43c308063cbfb08b26a9af50649dd877a876b75
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-24 09:43:49 +0100

    add all pages to nav

--- include/nav.js
+++ include/nav.js
@@ -1,6 +1,11 @@
+
 document.write(
     "<ul>"
-    + "<a href='admin.html'><div class='nav-slot'>Panel Admin</div></a>"
+    + "<a href='login.html'><div class='nav-slot'>Connexion</div></a>"
     + "<a href='system.html'><div class='nav-slot'>Supervision Système</div></a>"
+    + "<a href='admin.html'><div class='nav-slot'>Panel Admin</div></a>"
+    + "<a href='histoPassages.html'><div class='nav-slot'>Histo Passages</div></a>"
+    + "<a href='histoCapteurs.html'><div class='nav-slot'>Histo Capteurs</div></a>"
+    + "<a href='parametrage.html'><div class='nav-slot'>Paramétrage</div></a>"
     + "</ul>"
 );
