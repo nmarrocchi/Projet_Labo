12bb02159147500c54555a8177fe29fe1b32b8ec
nmarrocchi <nmarrocchi@la-providence.net>
2022-02-24 08:54:08 +0100

    Add js include nav

---
+++ include/nav.js
@@ -0,0 +1,6 @@
+document.write(
+    "<ul>"
+    + "<a href='admin.html'><div class='nav-slot'>Panel Admin</div></a>"
+    + "<a href='system.html'><div class='nav-slot'>Supervision Système</div></a>"
+    + "</ul>"
+);
