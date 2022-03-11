81d963d228f07e3fc705354f641fc1b44dc33dc8
nmarrocchi <nmarrocchi@la-providence.net>
2022-03-11 14:12:30 +0100

    Clement not compiling ( in comments )

--- Serveur/Securisation_Labo/security.cpp
+++ Serveur/Securisation_Labo/security.cpp
@@ -1,64 +1,64 @@
-#include "security.h"
-
-/* Constructeur */
-security::security()
-{
-
-	//this->db = db;
-
-}
-
-/* Destructeur */
-security::~security()
-{
-}
-
-/* Setup security class */
-void security::setSecurityById(int idSecurity)
-{
-}
-
-/* Setup security class by ID */
-void security::getIdSecurity()
-{
-	//selectdb();
-}
-
-/* Return the room in database */
-void security::getRoom()
-{
-}
-
-/* Return the bit in database */
-void security::getBit()
-{
-}
-
-/* Return the bit in database */
-void security::getStatut()
-{
-}
-
-/* Return a historic table of sensor/activator in database */
-void security::histoSecurity()
-{
-}
-
-/* Send the statut on the client */
-bool continuity::selectStatut()
-{
-
-	continuity continuity(room);
-
-	QString condition = "WHERE room = " + room;
-
-	db->selectdb(security_table, condition);
-
-	return this->statut = query.value(3).toBool();
-
-}
-
-/* Update the statut in database */
-void continuity::updateStatut()
-{
-}
+//#include "security.h"
+//
+///* Constructeur */
+//security::security()
+//{
+//
+//	//this->db = db;
+//
+//}
+//
+///* Destructeur */
+//security::~security()
+//{
+//}
+//
+///* Setup security class */
+//void security::setSecurityById(int idSecurity)
+//{
+//}
+//
+///* Setup security class by ID */
+//void security::getIdSecurity()
+//{
+//	//selectdb();
+//}
+//
+///* Return the room in database */
+//void security::getRoom()
+//{
+//}
+//
+///* Return the bit in database */
+//void security::getBit()
+//{
+//}
+//
+///* Return the bit in database */
+//void security::getStatut()
+//{
+//}
+//
+///* Return a historic table of sensor/activator in database */
+//void security::histoSecurity()
+//{
+//}
+//
+///* Send the statut on the client */
+//bool continuity::selectStatut()
+//{
+//
+//	continuity continuity(room);
+//
+//	QString condition = "WHERE room = " + room;
+//
+//	db->selectdb(security_table, condition);
+//
+//	return this->statut = query.value(3).toBool();
+//
+//}
+//
+///* Update the statut in database */
+//void continuity::updateStatut()
+//{
+//}
--- Serveur/Securisation_Labo/security.h
+++ Serveur/Securisation_Labo/security.h
@@ -1,93 +1,94 @@
-#pragma once
-
-#include "BDD.h"
-
-class security
-{
-
-	Q_OBJECT
-		BDD *db;
-
-	public:
-
-		/* Constructeur */
-		security();
-
-		/* Destructeur */
-		~security();
-
-	private slots:
-		
-		/* Setup security class */
-		void setSecurity();
-
-		/* Setup security class by ID */
-		void setSecurityById(int idSecurity);
-
-		/* Return the ID in database */
-		void getIdSecurity();
-
-		/* Return the room in database */
-		void getRoom();
-
-		/* Return the bit in database */
-		void getBit();
-
-		/* Return the statut in database */
-		void getStatut();
-
-		/* Return a historic table of sensor/activator in database */
-		void histoSecurity();
-
-		/* Send the statut on the client */
-		virtual bool selectStatut() = 0;
-
-		/* Update the statut in database */
-		virtual void updateStatut() = 0;
-
-	protected:
-
-		int idSecurity;
-
-		int room;
-
-		int bit;
-
-		bool statut;
-
-};
-
-class continuity : public security
-{
-
-public:
-
-	continuity(int room)
-	{
-		this->room = room;
-	}
-
-	virtual bool selectStatut();
-
-	virtual void updateStatut();
-
-private:
-
-	int room;
-
-};
-
-class sensor : public security
-{
-
-};
-
-class tamper : public security
-{
-
-};
-
-class presence : public security
-{
-
-};
+//#pragma once
+//
+//#include "BDD.h"
+//
+//class security
+//{
+//
+//
+//	private:
+//		BDD * db;
+//
+//	public:
+//
+//		/* Constructeur */
+//		security();
+//
+//		/* Destructeur */
+//		~security();
+//
+//	private slots:
+//		
+//		/* Setup security class */
+//		void setSecurity();
+//
+//		/* Setup security class by ID */
+//		void setSecurityById(int idSecurity);
+//
+//		/* Return the ID in database */
+//		void getIdSecurity();
+//
+//		/* Return the room in database */
+//		void getRoom();
+//
+//		/* Return the bit in database */
+//		void getBit();
+//
+//		/* Return the statut in database */
+//		void getStatut();
+//
+//		/* Return a historic table of sensor/activator in database */
+//		void histoSecurity();
+//
+//		/* Send the statut on the client */
+//		virtual bool selectStatut() = 0;
+//
+//		/* Update the statut in database */
+//		virtual void updateStatut() = 0;
+//
+//	protected:
+//
+//		int idSecurity;
+//
+//		int room;
+//
+//		int bit;
+//
+//		bool statut;
+//
+//};
+//
+//class continuity : public security
+//{
+//
+//public:
+//
+//	continuity(int room)
+//	{
+//		this->room = room;
+//	}
+//
+//	virtual bool selectStatut();
+//
+//	virtual void updateStatut();
+//
+//private:
+//
+//	int room;
+//
+//};
+//
+//class sensor : public security
+//{
+//
+//};
+//
+//class tamper : public security
+//{
+//
+//};
+//
+//class presence : public security
+//{
+//
+//};
