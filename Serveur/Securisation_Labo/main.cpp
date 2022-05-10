17f86df0a0f9cab6cd940248962b61f8fc7d2b03
nmarrocchi <nmarrocchi@la-providence.net>
2022-05-10 11:45:24 +0200

    reorder main

--- Serveur/Securisation_Labo/main.cpp
+++ Serveur/Securisation_Labo/main.cpp
@@ -1,5 +1,6 @@
 #include <QtCore/QCoreApplication>
 
+#include "user.h"
 #include "database.h"
 #include "security.h"
 #include "continuity.h"
@@ -12,14 +13,16 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-	//user * User = new user();
+	security::superviseTable(); // Affiche un tableau pour les états des capteurs
+
+	database * db = new database();
+	user * User = new user(db);
+
 	//Server * server = new Server();
 
-	//User->setUserByIdCard("1477334037");
+	User->setUserByIdCard("1063727088"); // Check la validité de la carte
 
-	security::superviseTable();
-
-	securitySysteme::getInstance();
+	securitySysteme::getInstance(db);	// Affiche les états des capteurs
 
     return a.exec();
 }
