3ab38007662904d33cc06b83802716d79f76c6c6
Clement-Cauet <70692830+Clement-Cauet@users.noreply.github.com>
2022-05-16 23:36:09 +0200

    Creation Websocket client

--- Client/src/include/nav.js
+++ Client/src/include/nav.js
@@ -1,11 +1,8 @@
 
 document.write(
     "<ul>"
-    + "<a href='login.html'><div class='nav-slot'>Connexion</div></a>"
-    + "<a href='system.html'><div class='nav-slot'>Supervision Système</div></a>"
-    + "<a href='admin.html'><div class='nav-slot'>Panel Admin</div></a>"
-    + "<a href='histoPassages.html'><div class='nav-slot'>Histo Passages</div></a>"
-    + "<a href='histoCapteurs.html'><div class='nav-slot'>Histo Capteurs</div></a>"
-    + "<a href='parametrage.html'><div class='nav-slot'>Paramétrage</div></a>"
+    + "<a href='supervision.html'><div class='nav-slot'>Supervision Système</div></a>"
+    + "<a href='admin.html'><div class='nav-slot'>Panel Administration</div></a>"
+    + "<a href=''><div class='nav-slot'>Déconnexion</div></a>"
     + "</ul>"
 );
--- Client/src/scripts/functions.js
+++ Client/src/scripts/functions.js
@@ -1,4 +1,7 @@
-// - Functions Marrocchi
+var socket = new WebSocket("ws://127.0.0.1:1234");
+
+var inputConnexion = document.getElementById('inputConnexion');
+
 function formRequest(){}
 
 function getCapteursHisto(){}
@@ -13,13 +16,73 @@ function createUser(){}
 
 function updateUser(){}
 
+inputConnexion.addEventListener('click', function() 
+{
+    
+    var login = inputEmail.value;
+    var mdp = inputPassword.value;
+
+    data = "Auth"+login+";"+mdp;
+
+    //socket.send(data);
+
+});
+
+socket.onopen = function() 
+{
+    
+    console.log("Client WebSocket: Nouvelle connexion");
+
+};
+
+socket.onclose = function() 
+{
+    
+    console.log("Client WebSocket: Deconnexion");
+
+};
+
+socket.onerror = function(error) 
+{
+    
+    console.error(error);
+
+};
+
+socket.onmessage = function(event) 
+{
+    
+    switch (event.data)
+    {
+
+        case 'AuthOk':
+            connexion();
+            break;
+        
+        case '':
+        
+        default:
+
+    }
+    /*if(event.data == "Authtrue" || event.data == "Salttrue"){
+        connexion.hidden = true;
+        inscription.hidden = true;
+        message.hidden = false;
+    }else{
+        var messageText = document.getElementById('messageText')
+        var messageDiv = document.createElement("div");
+        var messageContent = event.data;
+        messageDiv.innerHTML = "<p>"+messageContent+"</p>";
+        messageText.appendChild(messageDiv);
+    }*/
+
+};
+
 function connexion(){}
 
 function disconnection(){}
 
-// - Functions Colson
 function systemParameters(){}
 
-// - Functions Cauet
 function getAllCapteursState(){}
 
