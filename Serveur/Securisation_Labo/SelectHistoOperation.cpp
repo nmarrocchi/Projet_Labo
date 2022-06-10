9af1b0aa9573d8060c5b43815fdcf3fa2a511a6e
Clement-Cauet <cauet.clement@gmail.com>
2022-06-10 13:23:46 +0200

    Change format date value

--- Serveur/Securisation_Labo/SelectHistoOperation.cpp
+++ Serveur/Securisation_Labo/SelectHistoOperation.cpp
@@ -11,7 +11,7 @@ SelectHistoOperation::SelectHistoOperation(QWebSocket * ws)
 void SelectHistoOperation::run()
 {
 	QSqlQuery query;
-	query.exec("SELECT security.room, security.byte, historical.statut, historical.date FROM historical, security WHERE security.idSecurity = historical.idSecurity ORDER BY date DESC LIMIT 50");
+	query.exec("SELECT security.room, security.byte, historical.statut, DATE_FORMAT(historical.date, '%d/%c/%Y %T') FROM historical, security WHERE security.idSecurity = historical.idSecurity ORDER BY date DESC LIMIT 50");
 
 	while (query.next())
 	{
@@ -34,7 +34,7 @@ void SelectHistoOperation::run()
 		webServer::getInstance()->addOperation(new SendHistoResultOperation(ws, resultHisto));
 	}
 
-	query.exec("SELECT passage.idCard, user.mail, passage.date FROM `passage`, `user` WHERE passage.idCard = user.idCard ORDER BY date DESC LIMIT 50");
+	query.exec("SELECT passage.idCard, user.mail, DATE_FORMAT(passage.date, '%d/%c/%Y %T') FROM `passage`, `user` WHERE passage.idCard = user.idCard ORDER BY date DESC LIMIT 50");
 
 	while (query.next())
 	{
